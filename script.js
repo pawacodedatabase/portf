f2924082b92b8e9dc39d7d1d4f5fb4420383ea38
DamilolaHQ <Whakhydoh@gmail.com>
2024-05-04 19:41:31 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+/////////// hamburger ///////////////////
+
+
+
+
+const nav = document.querySelector('ul');
+const menu = document.querySelector('span.ham');
+
+
+menu.addEventListener('click', ()=> {
+    if (!nav.classList.contains('ulVisible')){
+        nav.classList.add('ulVisible')
+        menu.innerHTML = 'close'
+    }
+else{
+    nav.classList.remove('ulVisible')
+    menu.innerHTML = 'menu'
+}}) 
+
+/////////////// footer nav ////////////////////
+
+
+const fnlist = document.querySelectorAll('.list');
+function activeLink(){
+    fnlist.forEach((item) => 
+        item.classList.remove('active'));
+        this.classList.add('active')   
+    }
+    fnlist.forEach((item) => 
+item.addEventListener('click', activeLink))
+
+
+    ///////button event//////////
+
+
+    let hire = document.getElementById('hirebtn')
+
+    hire.addEventListener ('click', ()=> {
+        hire.innerText = 'THANK YOU!';
+    })
+
+    let con = document.getElementById('conbutton')
+
+    con.addEventListener ('click', ()=> {
+        con.innerText = 'THANK YOU!';
+    })
